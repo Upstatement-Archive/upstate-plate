d58e14c9e69b8d0dd74bf2140b7078ada8cec372
Michael Swartz <mswartz@gmail.com>
2013-05-24 12:59:52 -0400

    fixed the media grid fiasco in layout.scss

--- _js/up-base.js
+++ _js/up-base.js
@@ -38,10 +38,10 @@ var upBase;
 		}
 		$("table tr:odd").addClass("odd");
 		//nth-child fixes for media-grid
-		$(".media-block:nth-child(2n+3)").addClass("n2");
-		$(".media-block:nth-child(3n+4)").addClass("n3");
-		$(".media-block:nth-child(4n+5)").addClass("n4");
-		$(".media-block:nth-child(5n+6)").addClass("n5");
+		$(".media-block:nth-child(2n+3)").addClass("n3");
+		$(".media-block:nth-child(3n+4)").addClass("n4");
+		$(".media-block:nth-child(4n+5)").addClass("n5");
+		$(".media-block:nth-child(5n+6)").addClass("n6");
 	}
 
 	UpBase.prototype.initTabs = function(){
